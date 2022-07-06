39ba9ef7b9fdee7832ae289733036b3a2fb5da7a
Dzaky Fadli Firmansyah <zaaaafl654@gmail.com>
2022-07-06 15:42:09 +0700

    to table

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -5,140 +5,48 @@
 <div class="shopping-cart section">
     <div class="container">
         <div class="cart-list-head">
-            <!-- Cart List Title -->
-            <div class="cart-list-title">
-                <div class="row">
-                    <div class="col-lg-1 col-md-1 col-12">
-
-                    </div>
-                    <div class="col-lg-4 col-md-3 col-12">
-                        <p>Product Name</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>Quantity</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>Subtotal</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>Discount</p>
-                    </div>
-                    <div class="col-lg-1 col-md-2 col-12">
-                        <p>Remove</p>
-                    </div>
-                </div>
+            <div class="table-responsive">
+                <table class="table table-striped align-middle">
+                    <thead class="table-light text-center">
+                        <tr>
+                            <th style="width: 15%">Gambar</th>
+                            <th style="width: 30%">Nama</th>
+                            <th style="width: 15%">Jumlah</th>
+                            <th style="width: 15%">Harga</th>
+                            <th style="width: 10%">Hapus</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        <tr>
+                            <td>
+                                <img src="/img/KYT-C5-IANONE-WHITE.jpeg" alt="" class="img-thumbnail" style="max-height: 100px; max-width: 100px;">
+                            </td>
+                            <td>
+                                <h6 class="product-name"><a href="#">KYT C5 IANONE WHITE</a></h6>
+                            </td>
+                            <td>
+                                <div class="d-block quantity">
+                                    <div class="input-group ">
+                                        <button class="btn btn-outline-primary quantity-minus p-0 px-1" type="button">
+                                            <i class="fa-solid fa-minus"></i>
+                                        </button>
+                                        <input type="number" id="quantity" name="quantity" class="form-control text-center p-0" value="1" min="1" max="100">
+                                        <button class="btn btn-outline-primary quantity-plus p-0 px-1" type="button">
+                                            <i class="fa-solid fa-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <p>Rp.<span>1.700.000</span></p>
+                            </td>
+                            <td>
+                                <i class="fa-solid fa-circle-xmark"></i>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <!-- End Cart List Title -->
-            <!-- Cart Single List list -->
-            <div class="cart-single-list">
-                <div class="row align-items-center">
-                    <div class="col-lg-1 col-md-1 col-12">
-                        <a href="product-details.html"><img src="https://via.placeholder.com/220x200" alt="#"></a>
-                    </div>
-                    <div class="col-lg-4 col-md-3 col-12">
-                        <h5 class="product-name"><a href="product-details.html">
-                                Canon EOS M50 Mirrorless Camera</a></h5>
-                        <p class="product-des">
-                            <span><em>Type:</em> Mirrorless</span>
-                            <span><em>Color:</em> Black</span>
-                        </p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <div class="count-input">
-                            <select class="form-control">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>$910.00</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>$29.00</p>
-                    </div>
-                    <div class="col-lg-1 col-md-2 col-12">
-                        <a class="remove-item" href="javascript:void(0)"><i class="lni lni-close"></i></a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Single List list -->
-            <!-- Cart Single List list -->
-            <div class="cart-single-list">
-                <div class="row align-items-center">
-                    <div class="col-lg-1 col-md-1 col-12">
-                        <a href="product-details.html"><img src="https://via.placeholder.com/220x200" alt="#"></a>
-                    </div>
-                    <div class="col-lg-4 col-md-3 col-12">
-                        <h5 class="product-name"><a href="product-details.html">
-                                Apple iPhone X 256 GB Space Gray</a></h5>
-                        <p class="product-des">
-                            <span><em>Memory:</em> 256 GB</span>
-                            <span><em>Color:</em> Space Gray</span>
-                        </p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <div class="count-input">
-                            <select class="form-control">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>$1100.00</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>—</p>
-                    </div>
-                    <div class="col-lg-1 col-md-2 col-12">
-                        <a class="remove-item" href="javascript:void(0)"><i class="lni lni-close"></i></a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Single List list -->
-            <!-- Cart Single List list -->
-            <div class="cart-single-list">
-                <div class="row align-items-center">
-                    <div class="col-lg-1 col-md-1 col-12">
-                        <a href="product-details.html"><img src="https://via.placeholder.com/220x200" alt="#"></a>
-                    </div>
-                    <div class="col-lg-4 col-md-3 col-12">
-                        <h5 class="product-name"><a href="product-details.html">HP LaserJet Pro Laser Printer</a></h5>
-                        <p class="product-des">
-                            <span><em>Type:</em> Laser</span>
-                            <span><em>Color:</em> White</span>
-                        </p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <div class="count-input">
-                            <select class="form-control">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>$550.00</p>
-                    </div>
-                    <div class="col-lg-2 col-md-2 col-12">
-                        <p>—</p>
-                    </div>
-                    <div class="col-lg-1 col-md-2 col-12">
-                        <a class="remove-item" href="javascript:void(0)"><i class="lni lni-close"></i></a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Single List list -->
         </div>
         <div class="row">
             <div class="col-12">
