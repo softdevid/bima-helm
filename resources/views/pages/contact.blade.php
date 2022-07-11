f05456a0613f6b9b80bb1dfa0defad364ba17bce
Ardianto <ardianto8306@gmail.com>
2022-07-11 16:09:21 +0700

    contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main')
 @section('content')
-    <div class="container">
+    <div class="container text-dark">
+        <div class="section-title">
+            <h2>Kontak Kami</h2>
+        </div>
         <div class="row">
             <div class="col-12">
                 <article>
@@ -18,8 +21,9 @@
                                             </div>
                                             <div class="col-md-8 text-md-start text-center">
                                                 <div class="card-body">
-                                                    <h5 class="card-title">+62 857-2806-0268</h5>
-                                                    <p class="card-text">Phone number of Company</p>
+                                                    <a href="telp:085728060268">
+                                                        <h5 class="card-title">0857-2806-0268</h5>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
@@ -34,15 +38,16 @@
                                             </div>
                                             <div class="col-md-8 text-md-start text-center">
                                                 <div class="card-body">
-                                                    <h5 class="card-title">mekarlaserc@gmail.com</h5>
-                                                    <p class="card-text">For technical issues and requests.</p>
+                                                    <a href="mailto:bimahelm@gmail.com" target="_blank">
+                                                        <h5 class="card-title">bimahelm@gmail.com</h5>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="card border-0 mb-3" style="max-width: 540px;">
                                         <div class="row g-0">
-                                            <div class="col-md-4 text-center">
+                                            <div class="col-md-4 text-center mt-4">
                                                 <span class="fa-stack" style="vertical-align: top; font-size: 40px">
                                                     <i class="fa-regular fa-circle fa-stack-2x"></i>
                                                     <i class="fa-solid fa-building fa-stack-1x"></i>
@@ -50,20 +55,23 @@
                                             </div>
                                             <div class="col-md-8 text-md-start text-center">
                                                 <div class="card-body">
-                                                    <h5 class="card-title">Mekar Cutting Digital Head Office</h5>
-                                                    <p class="card-text">Kalikabong, Kec. Kalimanah.
-                                                        <br>Purbalingga, Jawa Tengah.<br>53321
-                                                    </p>
+                                                    <a
+                                                        href="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3956.6256419545593!2d109.3523764!3d-7.3957763!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6559b5c54a1705%3A0x8b8919753a714d3b!2sBima%20Helm%20%26%20Accessories!5e0!3m2!1sid!2sid!4v1657527106619!5m2!1sid!2sid">
+                                                        <h5 class="card-title">Jl. Ahmad Yani No.76, Kandang Gampang,
+                                                            Kalikabong, Kec. Kalimanah, Kabupaten Purbalingga, Jawa Tengah
+                                                        </h5>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-7" style="padding-left: 15px; padding-right: 15px;">
-                                    <div class="p-md-3 form-talk">
+                                <div class="col-lg-7"
+                                    style="padding-left: 15px; padding-right: 15px; padding-bottom: 10px;">
+                                    <div class="p-md-3">
                                         <iframe
-                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.625036234294!2d109.350116714322!3d-7.395843874867294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e65590298da8fe9%3A0x9be041f315dc3785!2sJasa%20Akrilik%20dan%20Neon%20Box%20Purbalingga%20-%20Mekar%20Laser%20Cutting!5e0!3m2!1sid!2sid!4v1657162371069!5m2!1sid!2sid"
-                                            style="border:0; width: 100%; height:450px; min-width: 100%;" allowfullscreen=""
+                                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3956.6256419545593!2d109.3523764!3d-7.3957763!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6559b5c54a1705%3A0x8b8919753a714d3b!2sBima%20Helm%20%26%20Accessories!5e0!3m2!1sid!2sid!4v1657527106619!5m2!1sid!2sid"
+                                            style="border:0; width: 100%; height:400px; min-width: 100%;" allowfullscreen=""
                                             loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                     </div>
                                 </div>
