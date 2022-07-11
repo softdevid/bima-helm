d564a183e3d697be6f2afaad9c9cc0ca5df355ba
Ardianto <ardianto8306@gmail.com>
2022-07-11 16:04:42 +0700

    contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,147 +1,77 @@
 @extends('layouts.main')
 @section('content')
-    <div class="content text-dark">
-        <main class="container">
-            <div class="talk-title-wrap">
-                <div class="container">
-                    <div class="row">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <article>
+                    <div class="talk-content">
                         <div class="col-12">
-                            <div class="section-title mt-3">
-                                <h2 class="judul-talk">Kontak</h2>
+                            <div class="row" style="margin-left: -15px;margin-right: -15px;">
+                                <div class="col-lg-5 stack1" style="padding-left: 15px; padding-right: 15px;">
+                                    <div class="card border-0 mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 text-center">
+                                                <span class="fa-stack" style="vertical-align: top; font-size: 40px">
+                                                    <i class="fa-regular fa-circle fa-stack-2x"></i>
+                                                    <i class="fa-solid fa-phone fa-stack-1x"></i>
+                                                </span>
+                                            </div>
+                                            <div class="col-md-8 text-md-start text-center">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">+62 857-2806-0268</h5>
+                                                    <p class="card-text">Phone number of Company</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card border-0 mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 text-center">
+                                                <span class="fa-stack" style="vertical-align: top; font-size: 40px">
+                                                    <i class="fa-regular fa-circle fa-stack-2x"></i>
+                                                    <i class="fa-solid fa-envelope fa-stack-1x"></i>
+                                                </span>
+                                            </div>
+                                            <div class="col-md-8 text-md-start text-center">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">mekarlaserc@gmail.com</h5>
+                                                    <p class="card-text">For technical issues and requests.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card border-0 mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 text-center">
+                                                <span class="fa-stack" style="vertical-align: top; font-size: 40px">
+                                                    <i class="fa-regular fa-circle fa-stack-2x"></i>
+                                                    <i class="fa-solid fa-building fa-stack-1x"></i>
+                                                </span>
+                                            </div>
+                                            <div class="col-md-8 text-md-start text-center">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Mekar Cutting Digital Head Office</h5>
+                                                    <p class="card-text">Kalikabong, Kec. Kalimanah.
+                                                        <br>Purbalingga, Jawa Tengah.<br>53321
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7" style="padding-left: 15px; padding-right: 15px;">
+                                    <div class="p-md-3 form-talk">
+                                        <iframe
+                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.625036234294!2d109.350116714322!3d-7.395843874867294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e65590298da8fe9%3A0x9be041f315dc3785!2sJasa%20Akrilik%20dan%20Neon%20Box%20Purbalingga%20-%20Mekar%20Laser%20Cutting!5e0!3m2!1sid!2sid!4v1657162371069!5m2!1sid!2sid"
+                                            style="border:0; width: 100%; height:450px; min-width: 100%;" allowfullscreen=""
+                                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </article>
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <article>
-                            <div class="talk-content">
-                                <div class="col-12">
-                                    <div class="row" style="margin-left: -15px;margin-right: -15px;">
-                                        <div class="col-lg-5 stack1 pt-5" style="padding-left: 15px; padding-right: 15px;">
-                                            <div class="card border-0 mb-3" style="max-width: 540px;">
-                                                <div class="row g-0">
-                                                    <div class="col-md-4 text-center">
-                                                        <span class="fa-stack" style="vertical-align: top; font-size: 40px">
-                                                            <i class="fa-regular fa-circle fa-stack-2x"></i>
-                                                            <i class="fa-solid fa-phone fa-stack-1x"></i>
-                                                        </span>
-                                                    </div>
-                                                    <div class="col-md-8 text-md-start text-center">
-                                                        <div class="card-body">
-                                                            <h5 class="card-title">0857-2806-0268</h5>
-                                                            {{-- <p class="card-text">Phone number of Company</p> --}}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="card border-0 mb-3" style="max-width: 540px;">
-                                                <div class="row g-0">
-                                                    <div class="col-md-4 text-center">
-                                                        <span class="fa-stack mt-3"
-                                                            style="vertical-align: top; font-size: 40px">
-                                                            <i class="fa-regular fa-circle fa-stack-2x"></i>
-                                                            <i class="fa-solid fa-location-dot fa-stack-1x"></i>
-                                                        </span>
-                                                    </div>
-                                                    <div class="col-md-8 text-md-start text-center">
-                                                        <div class="card-body">
-                                                            <h5 class="card-title">Jl. Ahmad Yani No.76, Kandang Gampang,
-                                                                Kalikabong, Kec. Kalimanah, Kabupaten Purbalingga, Jawa
-                                                                Tengah</h5>
-                                                            {{-- <p class="card-text">Location of Company</p> --}}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="card border-0 mb-3" style="max-width: 540px;">
-                                                <div class="row g-0">
-                                                    <div class="col-md-4 text-center">
-                                                        <span class="fa-stack" style="vertical-align: top; font-size: 40px">
-                                                            <i class="fa-regular fa-circle fa-stack-2x"></i>
-                                                            <i class="fa-solid fa-envelope fa-stack-1x"></i>
-                                                        </span>
-                                                    </div>
-                                                    <div class="col-md-8 text-md-start text-center">
-                                                        <div class="card-body">
-                                                            <h5 class="card-title">mekarlaserc@gmail.com</h5>
-                                                            {{-- <p class="card-text">For technical issues and requests.</p> --}}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-7" style="padding-left: 15px; padding-right: 15px;">
-                                            <div class="p-md-5">
-                                                <iframe
-                                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7913.203097569752!2d109.34314242239886!3d-7.398463689102401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6559b5c54a1705%3A0x8b8919753a714d3b!2sBima%20Helm%20%26%20Accessories!5e0!3m2!1sid!2sid!4v1657524670093!5m2!1sid!2sid"
-                                                    height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="reach-us">
-                                <h2 class="judul-lokasi" style="font-weight: bold;margin-bottom: 20px;">Reach Us Nearby
-                                </h2>
-                                <div class="reach-us-place">
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="card border-0 mb-3" style="max-width: 540px;">
-                                                <div class="row g-0">
-                                                    <div class="col-md-4 d-flex justify-content-center">
-                                                        <div class="d-flex justify-content-center align-items-center">
-                                                            <span class="fa-stack stack4"
-                                                                style="vertical-align: top; font-size: 40px">
-                                                                <i class="fa-regular fa-circle fa-stack-2x"></i>
-                                                                <i class="fa-solid fa-building fa-stack-1x"></i>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-8 text-md-start text-center">
-                                                        <div class="card-body isi-stack4">
-                                                            <h5 class="card-title">Mekar Cutting Digital Head Office</h5>
-                                                            <p class="card-text">Kalikabong, Kec. Kalimanah.
-                                                                <br>Purbalingga, Jawa Tengah.<br>53152
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <div class="card border-0 mb-3" style="max-width: 540px;">
-                                                <div class="row g-0">
-                                                    <div class="col-md-4 d-flex justify-content-center">
-                                                        <div class="d-flex justify-content-center align-items-center">
-                                                            <span class="fa-stack stack5"
-                                                                style="vertical-align: top; font-size: 40px">
-                                                                <i class="fa-regular fa-circle fa-stack-2x"></i>
-                                                                <i class="fa-solid fa-building fa-stack-1x"></i>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-8 text-md-start text-center">
-                                                        <div class="card-body isi-stack5">
-                                                            <h5 class="card-title">Mekar Cutting Digital Head Office</h5>
-                                                            <p class="card-text">Kalikabong, Kec. Kalimanah.
-                                                                <br>Purwokerto, Jawa Tengah.<br>53152
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </main>
+        </div>
     </div>
 @endsection
