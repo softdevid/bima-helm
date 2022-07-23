d690b4cd59db6d21086ded582866428f86e08663
Dzaky Fadli <zaaaafl654@gmail.com>
2022-07-23 14:14:31 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\AdminProductController;
 use App\Http\Controllers\RegionController;
 use App\Http\Controllers\MerekController;
 use Illuminate\Support\Facades\Route;
-use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 use phpDocumentor\Reflection\Types\Resource_;
 
 /*
@@ -26,7 +26,9 @@ use phpDocumentor\Reflection\Types\Resource_;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/login', [HomeController::class, 'login'])->middleware('guest')->name('login');
+Route::get('/login', [HomeController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
 Route::get('/register', [HomeController::class, 'register'])->middleware('guest');
@@ -45,45 +47,22 @@ Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/details/{product:slug}', [ProductController::class, 'detail']);
 
-Route::get('/cart', [CartNWishController::class, 'index']);
-Route::get('/cart-add', [CartNWishController::class, 'add']);
-Route::get('/fav', fn () => view('pages.cart', [
-    "title" => "Produk yang disukai"
-]))->middleware('auth');
+Route::get('/cart', [CartNWishController::class, 'indexCart']);
+Route::get('/cart-add', [CartNWishController::class, 'cartAdd']);
+Route::get('/cart-remove', [CartNWishController::class, 'cartRemove']);
+Route::get('/cart-removeAll', [CartNWishController::class, 'cartRemoveAll']);
+Route::get(
+    '/fav',
+    fn() => view('pages.cart', [
+        'title' => 'Produk yang disukai',
+    ]),
+)->middleware('auth');
 
 Route::get('/checkout', function () {
     return view('pages.checkout', [
-        "title" => "Checkout"
+        'title' => 'Checkout',
     ]);
 });
-
-//route admin
-Route::get('/admin/dashboard', function () {
-    return view('admin.pages.index', [
-        "title" => "Dashboard"
-    ]);
-});
-Route::resource('/admin/merek', MerekController::class)->middleware('auth');
-Route::get('/admin/product', [ProductAdminController::class, 'index']);
-Route::get('/admin/users', [AdminController::class, 'users']);
-Route::get('/admin/orders', [AdminController::class, 'orders']);
-Route::post('/admin/product/create', [ProductAdminController::class, 'store']);
-Route::get('/admin/product/create', [ProductAdminController::class, 'create']);
-Route::get('/admin/product/delete/{id}', [ProductAdminController::class, 'create']);
-// Route::resource('productadmin', ProductAdminController::class);
-
-//KASIR
-Route::get('/kasir/dashboard', function () {
-    return view('kasir.pages.index', [
-        "title" => "Dashboard"
-    ]);
-});
-Route::get('/kasir/laporan', function () {
-    return view('kasir.pages.laporan', [
-        "title" => "Laporan"
-    ]);
-});
-
 
 //route slug generate
 Route::get('check_slug', function () {
@@ -91,5 +70,30 @@ Route::get('check_slug', function () {
     return response()->json(['slug' => $slug]);
 });
 
-//rout admin product
-Route::resource('admin-product', AdminProductController::class);
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    //rout admin product
+    Route::resource('admin-product', AdminProductController::class);
+    //route admin
+    Route::get('/admin/dashboard', function () {
+        return view('admin.pages.index', [
+            'title' => 'Dashboard',
+        ]);
+    });
+    Route::resource('/admin/merek', MerekController::class)->middleware('auth');
+    Route::get('/admin/product', [ProductAdminController::class, 'index']);
+    Route::get('/admin/users', [AdminController::class, 'users']);
+    Route::get('/admin/orders', [AdminController::class, 'orders']);
+    // Route::resource('productadmin', ProductAdminController::class);
+
+    //KASIR
+    Route::get('/kasir/dashboard', function () {
+        return view('kasir.pages.index', [
+            'title' => 'Dashboard',
+        ]);
+    });
+    Route::get('/kasir/laporan', function () {
+        return view('kasir.pages.laporan', [
+            'title' => 'Laporan',
+        ]);
+    });
+});
