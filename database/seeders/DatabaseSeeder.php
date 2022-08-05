fa3b9e9bcc838f2973cf55acea5105cf6f91b225
zionexeonch <zionexeon@gmail.com>
2022-08-05 15:33:49 +0700

    red

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -70,119 +70,119 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
-        Product::create([
-            "category_id" => 1,
-            "name" => "HELM FULLFACE KTY RC SEVEN #14 YELLOW FLUO",
-            "slug" => "fullface-kyt-rc-seven-14-yellow-fluo",
-            "merk_id" => 1,
-            "price" => "420000",
-            "weight" => "2000",
-            "size_id" => 1,
-            "sold" => 3,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 1,
+        //     "name" => "HELM FULLFACE KTY RC SEVEN #14 YELLOW FLUO",
+        //     "slug" => "fullface-kyt-rc-seven-14-yellow-fluo",
+        //     "merk_id" => 1,
+        //     "price" => "420000",
+        //     "weight" => "2000",
+        //     "size_id" => 1,
+        //     "sold" => 3,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 1,
-            "name" => "HELM FULLFACE INK CL MAX WHITE YELLOW FLUO",
-            "slug" => "fullface-ink-cl-max-whiteyellow-fluo",
-            "merk_id" => 2,
-            "price" => "469000",
-            "weight" => "2000",
-            "size_id" => 2,
-            "sold" => 2,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 1,
+        //     "name" => "HELM FULLFACE INK CL MAX WHITE YELLOW FLUO",
+        //     "slug" => "fullface-ink-cl-max-whiteyellow-fluo",
+        //     "merk_id" => 2,
+        //     "price" => "469000",
+        //     "weight" => "2000",
+        //     "size_id" => 2,
+        //     "sold" => 2,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 1,
-            "name" => "KYT C5 IANONE WHITE",
-            "slug" => "fullface-kyt-c5-ianone-white",
-            "merk_id" => 1,
-            "price" => "2100000",
-            "weight" => "2000",
-            "size_id" => 3,
-            "sold" => 2,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 1,
+        //     "name" => "KYT C5 IANONE WHITE",
+        //     "slug" => "fullface-kyt-c5-ianone-white",
+        //     "merk_id" => 1,
+        //     "price" => "2100000",
+        //     "weight" => "2000",
+        //     "size_id" => 3,
+        //     "sold" => 2,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 2,
-            "name" => "KYT DJ MARU #5 BLACK RED MAROON",
-            "slug" => "halfface-kyt-djmaru-5-black-red-maroon",
-            "merk_id" => 1,
-            "price" => "350000",
-            "weight" => "2000",
-            "size_id" => 4,
-            "sold" => 1,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 2,
+        //     "name" => "KYT DJ MARU #5 BLACK RED MAROON",
+        //     "slug" => "halfface-kyt-djmaru-5-black-red-maroon",
+        //     "merk_id" => 1,
+        //     "price" => "350000",
+        //     "weight" => "2000",
+        //     "size_id" => 4,
+        //     "sold" => 1,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 1,
-            "name" => "KYT RC SEVEN #17 BLACK DOFT GOLD",
-            "slug" => "fullface-kyt-rc-seven-17-black-doft-gold",
-            "merk_id" => 1,
-            "price" => "420000",
-            "weight" => "2000",
-            "size_id" => 5,
-            "sold" => 4,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 1,
+        //     "name" => "KYT RC SEVEN #17 BLACK DOFT GOLD",
+        //     "slug" => "fullface-kyt-rc-seven-17-black-doft-gold",
+        //     "merk_id" => 1,
+        //     "price" => "420000",
+        //     "weight" => "2000",
+        //     "size_id" => 5,
+        //     "sold" => 4,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 1,
-            "name" => "KYT TT COURSE PLAIN MATT BLACK",
-            "slug" => "kyt-tt-course-plain-matt-black",
-            "merk_id" => 1,
-            "price" => "1700000",
-            "weight" => "2000",
-            "size_id" => 6,
-            "sold" => 5,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 1,
+        //     "name" => "KYT TT COURSE PLAIN MATT BLACK",
+        //     "slug" => "kyt-tt-course-plain-matt-black",
+        //     "merk_id" => 1,
+        //     "price" => "1700000",
+        //     "weight" => "2000",
+        //     "size_id" => 6,
+        //     "sold" => 5,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 3,
-            "name" => "Spoiler Helm KYT K2R",
-            "slug" => "spoiler-helm-kyt-k2r",
-            "merk_id" => null,
-            "price" => "70000",
-            "weight" => "100",
-            "size_id" => null,
-            "stock" => 20,
-            "sold" => 1121,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 3,
+        //     "name" => "Spoiler Helm KYT K2R",
+        //     "slug" => "spoiler-helm-kyt-k2r",
+        //     "merk_id" => null,
+        //     "price" => "70000",
+        //     "weight" => "100",
+        //     "size_id" => null,
+        //     "stock" => 20,
+        //     "sold" => 1121,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
-        Product::create([
-            "category_id" => 4,
-            "name" => "BUSA FULLSET HELM INK CX 22",
-            "slug" => "busa-fullset-helm-ink-cx-22",
-            "merk_id" => 2,
-            "price" => "50000",
-            "weight" => "500",
-            "size_id" => null,
-            "stock" => 5,
-            "sold" => 6,
-            'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
-            "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
-        ]);
+        // Product::create([
+        //     "category_id" => 4,
+        //     "name" => "BUSA FULLSET HELM INK CX 22",
+        //     "slug" => "busa-fullset-helm-ink-cx-22",
+        //     "merk_id" => 2,
+        //     "price" => "50000",
+        //     "weight" => "500",
+        //     "size_id" => null,
+        //     "stock" => 5,
+        //     "sold" => 6,
+        //     'image_main' => "HELM-FULLFACE-KYT-RC-SEVEN-14-YELLOW-FLUO.jpeg",
+        //     "url" => "https://res.cloudinary.com/cv-mekar-cutting-digital/image/upload/v1659326183/products/jntsanfausuhcfncpiuo.png",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fugiat voluptatum ut necessitatibus consequuntur. Consequuntur odio perferendis nobis maiores dolore! Veniam maiores nisi magnam sequi repellendus similique ut doloribus inventore!",
+        // ]);
 
         //Product::factory(20)->create();
         Image::factory(8)->create();
