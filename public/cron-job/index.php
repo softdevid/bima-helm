08fc182eb11f06eb5052dc78a46186e8412320c2
Dzaky Fadli <zaaaafl654@gmail.com>
2022-08-29 11:41:17 +0700

    re

--- public/cron-job/index.php
+++ public/cron-job/index.php
@@ -6,20 +6,24 @@ if ($mysqli->connect_errno) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
-$sql = "INSERT INTO daily_stocks SELECT
-NULL,
-categories.id AS category_id,
-COALESCE(SUM(sizes.xs+sizes.s+sizes.m+sizes.lg+sizes.xl+sizes.xxl), 0) as store,
-COALESCE(SUM(gudangs.xs+gudangs.s+gudangs.m+gudangs.lg+gudangs.xl+gudangs.xxl), 0) as storehouse,
-CURRENT_DATE() as date
-FROM products
-JOIN sizes ON products.size_id = sizes.id
-JOIN gudangs ON products.gudang_id = gudangs.id
-RIGHT JOIN categories ON products.category_id = categories.id
-GROUP BY categories.id;";
+if ($mysqli->query("ALTER TABLE daily_stocks AUTO_INCREMENT = 1")) {
+    $sql = "INSERT INTO daily_stocks SELECT
+    NULL,
+    categories.id AS category_id,
+    COALESCE(SUM(sizes.xs+sizes.s+sizes.m+sizes.lg+sizes.xl+sizes.xxl), 0) as store,
+    COALESCE(SUM(gudangs.xs+gudangs.s+gudangs.m+gudangs.lg+gudangs.xl+gudangs.xxl), 0) as storehouse,
+    CURRENT_DATE() as date
+    FROM products
+    JOIN sizes ON products.size_id = sizes.id
+    JOIN gudangs ON products.gudang_id = gudangs.id
+    RIGHT JOIN categories ON products.category_id = categories.id
+    GROUP BY categories.id;";
 
-if ($mysqli->query($sql)) {
-    echo json_encode(["message" => "success"]);
+    if ($mysqli->query($sql)) {
+        echo json_encode(["message" => "success"]);
+    } else {
+        echo json_encode(["message" => $mysqli->error]);
+    }
 } else {
     echo json_encode(["message" => $mysqli->error]);
 }
